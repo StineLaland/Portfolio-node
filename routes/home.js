835f27b5899dcb55453c83a63d29405680022de8
Stine Laland <laland.stine@gmail.com>
2024-08-20 12:44:06 +0200

    Refactor recommendations

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,9 @@
-var bodyParser = require('body-parser')
-var jsonParser = bodyParser.json()
 var express = require('express');
 var router = express.Router();
 const fs = require("fs")
 const path = require("path")
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,11 +12,22 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', jsonParser, function(req, res, next) {
-    let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
-    let array = JSON.parse(rawdata);
-    const newArray = array.concat([req.body.newText])
-    fs.writeFileSync(path.resolve(__dirname, "../data/introductionArray.json"), JSON.stringify(newArray));
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
+  let array = JSON.parse(rawdata);
+  const newArray = array.concat([req.body.newText])
+  fs.writeFileSync(path.resolve(__dirname, "../data/introductionArray.json"), JSON.stringify(newArray));
+  res.end();
+});
+
+router.delete('/', jsonParser, function(req, res, next) {
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
+  let array = JSON.parse(rawdata);
+  const index = array.indexOf(req.body.deletedText);
+  if (index > -1) {
+    array.splice(index, 1);
+    fs.writeFileSync(path.resolve(__dirname, "../data/introductionArray.json"), JSON.stringify(array));
     res.end();
-  });
+  }
+});
 
 module.exports = router;
